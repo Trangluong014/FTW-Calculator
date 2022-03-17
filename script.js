07a51e79a8ce86f0dc27a3fb7c19fa7f21a59a90
Trang Luong <luongtrang014@gmail.com>
2022-03-17 23:20:37 +0800

    update js

--- script.js
+++ script.js
@@ -4,15 +4,6 @@ let num2 = "";
 let operator = null;
 let previousButton = null;
 
-const handleBtnClick = (e) => {
-  if (operator) {
-    num2 += e.target.value;
-  } else {
-    num1 += e.target.value;
-  }
-  console.log({ num1, num2 });
-};
-
 const calculate = (num1, num2) => {
   num1 = parseInt(num1);
   num2 = parseInt(num2);
@@ -28,20 +19,41 @@ const calculate = (num1, num2) => {
   if ((operator = "x")) {
     result = num1 * num2;
   }
+  //console.log(operator);
   console.log(result);
+  display.textContent = result;
+};
+
+const clearCalc = () => {
+  (num1 = ""), (num2 = ""), (operator = null), (display["innerHTML"] = "");
+};
+
+const delbtn = () => {
+  (num1 = ""), (num2 = ""), (operator = null), (display["innerHTML"] = "");
+};
+
+const handleBtnClick = (e) => {
+  if (operator) {
+    num2 += e.target.innerText;
+    display.textContent = num2;
+  } else {
+    num1 += e.target.innerText;
+    display.textContent = num1;
+  }
 };
 
 const handleOperatorClick = (e) => {
-  if (e.target.textContent === "=") {
+  if (e.target.innerText === "=") {
     calculate(num1, num2);
-  } else if (operator && num2) {
-    calculate(num1, num2);
-    num1 = result;
-    num2 = "";
-    operator = e.target.textContent;
+    //} //else if (operator && num2) {
+    //num1 = result;
+    //num2 = "";
+    //calculate(num1, num2);
+    //operator = e.target.innerText;
   } else {
-    operator = e.target.textContent;
+    operator = e.target.innerText;
   }
+  console.log(operator);
 };
 
 const btnNumber = document.querySelectorAll(".number");
@@ -54,5 +66,8 @@ btnOperator.forEach((btnOperator) =>
   btnOperator.addEventListener("click", handleOperatorClick)
 );
 
+const btnClear = document.querySelector(".clear");
+btnClear.addEventListener("click", clearCalc);
+
 const btnDel = document.querySelector(".del");
-btnDel.addEventListener("click",delete)
+btnDel.addEventListener("click", delbtn);
