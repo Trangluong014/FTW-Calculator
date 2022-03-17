2e87023e95a3fb87e2ed4dcc9a4880a5abb0bc01
Trang Luong <luongtrang014@gmail.com>
2022-03-17 11:09:33 +0800

    update HTML CSS

---
+++ script.js
@@ -0,0 +1,9 @@
+let display = document.querySelector("#display");
+let num1 = "";
+let num2 = "";
+let operator = null;
+let previousButton = null;
+
+const addNumber = (num) => {
+  display.textContent = num;
+};
