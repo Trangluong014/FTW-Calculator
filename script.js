548263afbbe4677a0a6010ad571c2d180abe9adc
Trang Luong <luongtrang014@gmail.com>
2022-03-17 19:26:24 +0800

    update

--- script.js
+++ script.js
@@ -4,6 +4,52 @@ let num2 = "";
 let operator = null;
 let previousButton = null;
 
-const addNumber = (num) => {
-  display.textContent = num;
+const handleBtnClick = (e) => {
+  if (operator) {
+    num2 += e.target.value;
+  } else {
+    num1 += e.target.value;
+  }
+  console.log({ num1, num2 });
 };
+
+const calculate = (num1, num2) => {
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  if ((operator = "+")) {
+    result = num1 + num2;
+  }
+  if ((operator = "-")) {
+    result = num1 - num2;
+  }
+  if ((operator = "/")) {
+    result = num1 / num2;
+  }
+  if ((operator = "x")) {
+    result = num1 * num2;
+  }
+  console.log(result);
+};
+
+const handleOperatorClick = (e) => {
+  if (e.target.textContent === "=") {
+    calculate(num1, num2);
+  } else if (operator && num2) {
+    calculate(num1, num2);
+    num1 = result;
+    num2 = "";
+    operator = e.target.textContent;
+  } else {
+    operator = e.target.textContent;
+  }
+};
+
+const btnNumber = document.querySelectorAll(".number");
+btnNumber.forEach((btnNumber) =>
+  btnNumber.addEventListener("click", handleBtnClick)
+);
+
+const btnOperator = document.querySelectorAll(".operator");
+btnOperator.forEach((btnOperator) =>
+  btnOperator.addEventListener("click", handleOperatorClick)
+);
